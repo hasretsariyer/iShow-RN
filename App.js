edb90a658d8ae7f0de05cbfe3b756b07e6f8e6c2
Chen Mingliang <illuspas@msn.com>
2020-06-16 10:43:59 +0800

    add license key

--- App.js
+++ App.js
@@ -34,8 +34,8 @@ class HomeScreen extends React.Component {
     super(props);
     this.props = props;
     this.state = {
-      playserver: "rtmp://192.168.0.2/live/",
-      pushserver: "rtmp://192.168.0.2/live/",
+      playserver: "rtmps://192.168.0.101/live/",
+      pushserver: "rtmps://192.168.0.101/live/",
       stream: 'demo_' + (Math.floor(Math.random() * (999 - 100)) + 100),
     }
   }
@@ -43,6 +43,9 @@ class HomeScreen extends React.Component {
   componentWillMount() {
     if (Platform.OS === 'android') {
       requestCameraPermission()
+      NodeMediaClient.setLicense("ZjJhNTIzODAtNGU0ZDUzMjEtY24ubm9kZW1lZGlhLmlTaG93Uk4=-syY8+2t7utLZAKLDs1SaD0EOPC9ft3Zq2SncV7gvMg1vnuEGf6QYMDpiSWj0A7xLhbn62BJHJvi1sGLPKgRflHnT6ysuUfQM7W8fgMA75gbqSCMu4vVqssX+yWCeEIbb5uJ/WHYjSvjSOa0W69TwHB5OSxf0bgAMFo8oJjiSCG16CKRuCHeNQBF8KRh+PYuRDnd3pBmnvE8QyWMDpvtEJd1fSYrGLdwgeO8F4gBKoeXyk2/rpEHKDmm/MKAlHli0/mpz8ejlL6ifAw6rB0TqXfpUMuo6vXpx0bjV7G5wxnOMB5pubn91UWrpRoUhPjadOFiket1DmqPsZFiQGnv0iA==");
+    }else if(Platform.OS === 'ios') {
+      NodeMediaClient.setLicense("ZjJhNTIzODAtNGU0ZDUzMjEtY24ubm9kZW1lZGlhLmlTaG93Uk4=-CQ2OZOwxN8PmjPnqCO5jINgwytHewwXJgZ4OhYL0Hnh6TDjQJDL/ebvCV34cuN/LPn42+vEbKxVAhqv492V3RmNu2aPKL6+AlYtPNf1eWkFLYa9Q/5GwU22s98fKA6YB5IMQyG30VptasVRctQeIee/lhmGClkvo9Ib+C8rLai6HHzWst/WpfWJeJs9OYgosNcuS+VmydGAy/CkUkT4G2ew80q239GRSJ7g7KREcwgiPrGqPNiDFqtG1T08JD9SXELerQqIp71qaPRMjCDSk26L0Tg22z4/EKcp713bZGs2AnE3ye3RbsLdMfNNUU0j0Qc/PQFNpczkilbHwMDoRaA==");
     }
   }
 
